bfbdc9ea27abcb76d93155582ba0a24baef2a12a
kkimjjihhyeon <112775265+kkimjjihhyeon@users.noreply.github.com>
2022-11-12 00:50:53 +0900

    Add files via upload

---
+++ 파이썬 6장 8번 문제.py
@@ -0,0 +1,14 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+for i in range(10):
+    t.fd(-100)
+    t.lt(36)
+    t.fd(100)
+    t.lt(36)
+    t.fd(-100)
+    t.lt(36)
+    t.fd(100)
+    t.lt(36)
+    t.fd(-100)
+    t.rt(134)
